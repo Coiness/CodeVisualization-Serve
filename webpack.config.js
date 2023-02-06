e45516f77a365b32e619668d4243608ee0a2e4f2
60rzvvbj <1808078515@11.com>
2023-02-06 13:46:04 +0800

    feat: init webpack & ts

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+	target: 'node', // 运行环境为 node,
+
+	mode: 'none',
+	resolve: {
+		// Add '.ts' and '.tsx' as a resolvable extension.
+		extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+	},
+
+	module: {
+		rules: [
+			// all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	},
+};
+
+const server = {
+	...baseConfig,
+	name: 'main',
+	entry: './src/main.ts',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, './out')
+	},
+	plugins: [
+		new CleanWebpackPlugin()
+	]
+};
+
+module.exports = [server];
